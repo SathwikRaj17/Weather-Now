001d5afcfc79b54b23594c61d00b88ed82b82ec2
Sathwik Raj <sathwikraj003@gmail.com>
2024-02-15 23:10:50 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -1,92 +1,73 @@
-import express from "express"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import bodyparser from "body-parser"
-import env from "dotenv"
-const app=express();
-const port=3000;
-const pth=dirname(fileURLToPath(import.meta.url));
-const d=new Date();
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyparser from "body-parser";
+import env from "dotenv";
 
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(express.static("public"))
-app.set('view engine','ejs')
+const app = express();
+const port = 3000;
+const pth = dirname(fileURLToPath(import.meta.url));
+const d = new Date();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
 env.config();
 
-async function fetchd(location)
-{
-  
-    const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.apiid}`);
-    const data=await res.json();
+async function fetchd(location) {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.apiid}`);
+    const data = await res.json();
+    console.log(data.weather[0].main)
     return data;
 }
 
-app.listen(port,function(req,res)
-{
-    console.log(`listening on ${port}`)
-})
+app.listen(port, function (req, res) {
+    console.log(`listening on ${port}`);
+});
 
+app.get("/", function (req, res) {
+    res.sendFile(pth + "/public/index.html");
+});
 
-app.get("/",function(req,res)
-{
-    res.sendFile(pth+"/public/index.html")
-
-})
-
-function pt(condition)
-{
-    const time=d.getHours();
-    console.log(condition)
-    if(condition == "Rain" && time<18)
-    {
-        const path="rainy-day.mp4"
+function pt(condition) {
+    const time = d.getHours();
+    if (condition == "Rain" && time < 18) {
+        const path = "rainy-day.mp4";
         return path;
-    }
-    else if(condition =="Clear" && time<18)
-    {
-        const path="clear-day.mp4"
-        return path
-    }
-    else if(condition =="Clear" && time>=18)
-    {
-        const path="clear-night.mp4"
-        return path
-    }
-    else if(condition == "Rain" && time>=18)
-    {
-        const path="rainy-night.mp4"
-        return path
-    }
-    else if(time>=18)
-    {
-        const path="night.mp4"
-        return path
-    }
-    else if(time<18)
-    {
-        const path="day.mp4"
-        return path
+    } else if (condition == "Clear" && time < 18) {
+        const path = "clear-day.mp4";
+        return path;
+    } else if (condition == "Clear" && time >= 18) {
+        const path = "clear-night.mp4";
+        return path;
+    } else if (condition == "Rain" && time >= 18) {
+        const path = "rainy-night.mp4";
+        return path;
+    } else if (time >= 18) {
+        const path = "night.mp4";
+        return path;
+    } else if (time < 18) {
+        const path = "day.mp4";
+        return path;
     }
 }
 
-app.post("/search",async function(req,res)
-{
+app.post("/search", async function (req, res) {
     try {
-        const location=req.body.location;
-        const resp=await fetchd(location);
-        const condition=await(resp.weather[0].main);
-        console.log(resp.name)
-        const data={
-            temp:resp.main.temp,
-            min:resp.main.temp_min,
-            max:resp.main.temp_max,
-            speed:resp.wind.speed,
-            deg:resp.wind.deg,
-            path:pt(condition)
-        }
-        res.render("result.ejs",data);
+        const location = req.body.location;
+        const resp = await fetchd(location);
+        const condition = resp.weather[0].main;
+        const data = {
+            temp: resp.main.temp,
+            min: resp.main.temp_min,
+            max: resp.main.temp_max,
+            speed: resp.wind.speed,
+            deg: resp.wind.deg,
+            path: pt(condition)
+        };
+        res.render("result.ejs", data);
     } catch (error) {
         console.error(error);
-        res.sendFile(pth+"/public/index.html");
+        res.sendFile(pth + "/public/index.html");
     }
-})
+});
